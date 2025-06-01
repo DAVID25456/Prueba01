db921c737b430b2fb07775ee3cf52f89f0590932
LUIS DAVID <lm2023077088@virtual.upt.pe>
2025-05-31 21:40:47 -0500

    Commit 15

---
+++ PROYECTO_UNIDAD_02_MOSQUITO_SIERRA_CLARES_PEREZ_EPIS/Cliente/Menu principal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROYECTO_UNIDAD_02_MOSQUITO_SIERRA_CLARES_PEREZ_EPIS.Cliente
+{
+    public partial class Menu_principal : Form
+    {
+        public Menu_principal()
+        {
+            InitializeComponent();
+        }
+    }
+}
